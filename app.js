901c9e0781e0b00857876f99b2c8464f88289295
James Adams <jamesadams@Jamess-MacBook-Air.local>
2023-07-29 13:39:16 +0900

    finish

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+//nodejs da front da backendga request tashlay olishi uchun setheader bilan ruxsat berar edik
+//lekin bu express qilmaymiz expressda npm i cors qilib npm packed yuklab olamiz va shu orqali ruxsat beramiz;
+
 const express = require('express');
+const cors = require('cors');
 const Io = require('./utils/io');
 const Todo = require('./models/Todo');
 
@@ -6,16 +10,20 @@ const Todos = new Io('./database/todos.json');
 
 const app = express();
 const port = 4000;
-app.use(express.json());
-
-//get
+app.use(express.json()); //auto json ni parsa va stringify qilib beradi
+app.use(express.urlencoded({ extended: true })); //buni yozganimizni sababi front dan xozir from input ichidan turib togridan togri backendga post requestini yuboramiz shunda form json holatda kelmaydi aksincha urlencoded farmatda keladi expressni urlencoded metjhodi form inputdan kelgan datalrni parsalr beradi
+app.use(cors()); //cors function qaytadi // ozi default xolatida * ga yani barchaga ruxsat beradi
+//   app.use(cors({ origin: '127.0.0.1:5500/index.html' })); //1ta urlga dostup berish, lekin agar 1 nechta urlga dostup berish kerak bolsa  app.use(cors({origin:[url1],[url2],[url3]})) qilib ketsa boladi
+//get  frist step
 app.get('/', (req, res) => {
 	console.log('hello from backend');
 	res.send('Hello World!');
 });
 
-//post
+//post  creating
 app.post('/todo', async (req, res) => {
+	console.log(req.body);
+
 	const { title, description } = req.body;
 
 	if (!title || !description) {
@@ -37,6 +45,67 @@ app.post('/todo', async (req, res) => {
 	await Todos.write(data);
 
 	res.status(201).json({ message: 'Todo created successfuly' });
+});
+
+// get  reading
+app.get('/todos', async (req, res) => {
+	const todos = await Todos.read();
+	res.json({ todos });
+});
+
+//get find one
+app.get('/todo/:id', async (req, res) => {
+	const { id } = req.params;
+
+	const todos = await Todos.read();
+
+	const findTodo = todos.find(todo => todo.id == id);
+
+	res.json({ todo: findTodo });
+});
+
+//put means editing or updating
+
+app.put('/todo/:id', async (req, res) => {
+	const { id } = req.params; //put bilan param ishlatamiz bu body dan kelmaydi
+
+	const { title, description } = req.body;
+
+	const todos = await Todos.read();
+
+	const findTodo = todos.find(todo => todo.id == id);
+
+	if (!findTodo) {
+		return res.status(404).json({ message: 'Todo not found!' });
+	}
+
+	findTodo.title = title ? title : findTodo.title;
+	findTodo.description = description ? description : findTodo.description;
+
+	await Todos.write(todos); //reference ishlaydi ortada ozgartirgan narseng qoshib ketgan boladi
+
+	res.json({ message: 'successfuly updated' });
+});
+
+//delete
+
+app.delete('/todo/:id', async (req, res) => {
+	const { id } = req.params;
+
+	const { title, description } = req.body;
+
+	const todos = await Todos.read();
+	const findTodo = todos.find(todo => todo.id == id);
+
+	if (!findTodo) {
+		return res.status(404).json({ message: 'Todo not found!' });
+	}
+
+	const filterTodo = todos.filter(todo => todo.id != id);
+
+	await Todos.write(filterTodo);
+
+	res.json({ message: 'deleted succesfuly' });
 });
 
 app.all('/*', (req, res) => {
